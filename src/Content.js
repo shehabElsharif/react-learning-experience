601d65952f792e203a4f8df0d8cb97b29633d5e8
shehab <shehab.m.b.elsharif@gmail.com>
2022-11-08 20:55:29 +0200

    changed the button into a list and styled the list

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-/* import { useState } from 'react'; */
+import { useState } from 'react';
 
 const Content = () => {
 /*   const [name, setName] = useState('shehab');
@@ -14,11 +14,39 @@ const Content = () => {
         const name = handleNameChange();
         console.log(name);
         } */
+const [items, setItems] = useState([
+  {
+    id: 1,
+    checked: false,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+  {
+    id: 2,
+    checked: false,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+  {
+    id: 3,
+    checked: false,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+])
+
   return (
     <main>
-        {/* <button onClick={() =>{handleClick()}}> Hello {name}!</button> */}
-        
+      <ul>
+      {items.map((item) =>
+        <li className ="item" key={item.id}>
+          <input 
+          type="checkbox" 
+          checked="{item.checked}"
+          />
+          <label>{item.item}</label>
+          <button>Delete</button> 
+        </li>
 
+      )}
+    </ul>
     </main>
   )
   
