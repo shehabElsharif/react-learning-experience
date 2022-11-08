c0964893462ac90cc8a357afa9580e806400dc2e
shehab <shehab.m.b.elsharif@gmail.com>
2022-11-08 18:39:53 +0200

    made a button that changes its name per press.

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Content = () => {
+/*   const [name, setName] = useState('shehab');
     const handleNameChange =() =>{
         const name = ['bob', 'shehab', 'boushra'];
         const int = Math.floor(Math.random()*3);
+        setName(name[int]);
         return name[int];
       }
+      
+      const handleClick = () =>{
+        const name = handleNameChange();
+        console.log(name);
+        } */
   return (
     <main>
-        <button onClick={ console.log(handleNameChange())}>Hello </button>
+        {/* <button onClick={() =>{handleClick()}}> Hello {name}!</button> */}
         
 
     </main>
